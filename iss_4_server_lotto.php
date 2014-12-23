54c6e4ef32a9a34b7b1c7700604bbb4ea5b11df2
growingpain89 <growingpain89@gmail.com>
2014-12-23 14:26:52 +0900

    make example

--- iss_4_server_lotto.php
+++ iss_4_server_lotto.php
@@ -106,34 +106,4 @@
 */
 	}
 
-   //****** 모든 수행절차 종료 후 Json 형식으로 리턴시 사용
-   
-   /* 반환된 전체 레코드 수 저장.
-   $total_record = mysql_num_rows($result);
-	 
-   /* JSONArray 형식으로 만들기 위해서...
-   echo "{\"status\":\"OK\",\"num_results\":\"$total_record\",\"results\":[";
-
-   // 반환된 각 레코드별로 JSONArray 형식으로 만들기.
-   for ($i=0; $i < $total_record; $i++)                    
-   {
-      // 가져올 레코드로 위치(포인터) 이동  
-      mysql_data_seek($result, $i);       
-        
-      $row = mysql_fetch_array($result);
-   echo "{\"id\":$row[id]\"}";
- 
-   // 마지막 레코드 이전엔 ,를 붙인다. 그래야 데이터 구분이 되니깐.  
-   if($i<$total_record-1){
-      echo ",";
-   }
-    
-   }
-   
-   // JSONArray의 마지막 닫기
-   echo "]}";
-	
-	*/
-
-
 ?>
